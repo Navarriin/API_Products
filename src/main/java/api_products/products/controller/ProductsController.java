b69e7e5bc66623f3937152202267cb5b6a9ea418
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-15 18:41:43 -0300

    Finalizaçao do metodo PUT

--- src/main/java/api_products/products/controller/ProductsController.java
+++ src/main/java/api_products/products/controller/ProductsController.java
@@ -5,6 +5,7 @@ import api_products.products.entity.dto.ProductDTO;
 import api_products.products.entity.dto.ProductRequestDTO;
 import api_products.products.repository.ProductsRepositoty;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -43,6 +44,25 @@ public class ProductsController {
         Products productData = new Products(data);
         repository.save(productData);
         return ResponseEntity.ok(productData);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity putProduct(@PathVariable Long id, @RequestBody ProductDTO data) {
+        Optional<Products> optionalProducts = repository.findById((id));
+
+        if(optionalProducts.isPresent()){
+            Products products = optionalProducts.get();
+
+            products.setSku(data.sku());
+            products.setProduct(data.product());
+            products.setStock(data.stock());
+            products.setCost(data.cost());
+            products.setPrice(data.price());
+            return ResponseEntity.ok(products);
+        }else {
+            throw new EntityNotFoundException();
+        }
     }
 
     @DeleteMapping("/{id}")
